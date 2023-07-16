5493fccd37ff3fdd471eaa4e7b2a3794a59510cd
SirJoe <me@joehighton.com>
2023-07-16 20:35:07 +0100

    Create xmrig_setup.sh

---
+++ xmrig_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update and upgrade
+sudo apt update && sudo apt full-upgrade
+
+# Install dependencies
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+
+# Clone repository
+git clone https://github.com/xmrig/xmrig.git
+
+# Navigate to xmrig directory
+cd xmrig
+
+# Create build directory
+mkdir build
+
+# Navigate to build directory
+cd build
+
+# Run cmake
+cmake ..
+
+# Build xmrig
+make
